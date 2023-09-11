47e8b81f675a12cbcb827a65360dc0ef9211eed7
Moshezauros <moshe@cloudshare.com>
2023-09-11 16:10:30 +0300

    init

---
+++ GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7199");
+var client = new Greeter.GreeterClient(channel);
+Metadata headers = new()
+{
+    { "requestingUserId", "234" }
+};
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" },
+                  headers: headers
+                  );
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
